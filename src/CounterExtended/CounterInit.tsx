198005051c6ac9cee04414663b390efc70a60efc
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-08 19:19:38 +0300

    feat: add counterinit component

--- src/CounterExtended/CounterInit.tsx
+++ src/CounterExtended/CounterInit.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
+import s from "./Counter.module.css";
+import Button from "./Button";
+import InitInput from "./InitInput";
+import {InitError, InitType} from "./CounterExtended";
+type CounterInitPropsType = {
+    start: number
+    stop: number
+    setCallback: () => void
+    initialCallback: (i: InitType, v: string) => void
+    error: InitError
 
-const CounterInit = () => {
+}
+
+const CounterInit = ({start,stop,error, setCallback, initialCallback}:CounterInitPropsType) => {
+    const isSettable = localStorage.getItem('counter') === JSON.stringify({start: start, stop: stop})
+    console.log(isSettable)
     return (
-        <div>
-           init
+        <div className={s.counter}>
+            <div className={s.monitor}>
+                <div className={s.monitorColumn}>
+                    <p>start</p>
+                   <InitInput init={'start'} value={start} callback={initialCallback} error={error} />
+                    <p>end</p>
+                   <InitInput init={'stop'} value={stop} callback={initialCallback} error={error} />
+                </div>
+            </div>
+            <div className={s.buttonsContainer}>
+                <Button name={'set'} callback={setCallback} disabled={isSettable}/>
+            </div>
+
         </div>
     );
-};
-
+}
 export default CounterInit;
