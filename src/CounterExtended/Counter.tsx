e8469ff925684842cd024db76a88371d30f9f235
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-08 19:19:51 +0300

    feat: add counter component

--- src/CounterExtended/Counter.tsx
+++ src/CounterExtended/Counter.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import styles from './Counter.module.css'
+import s from './Counter.module.css'
+import Button from "./Button";
+import {validateHeaderName} from "http";
 type CounterPropsType = {
     counter: number
-    startValue: number
-    stopValue: number
-    error: string
+    increment: () => void
+    reset: () => void
+    error: initError
 }
-const Counter = ({counter,startValue, stopValue, error}: CounterPropsType) => {
-    return (
-        <div className={styles.counter}>
-            <div className={styles.monitor}>
+type initError = {
+    [key: string]: string
+}
+const Counter = ({counter,increment, reset, error}: CounterPropsType) => {
+   const monitorStyle = Object.values(error).filter(e => !!e).length ? `${s.monitor} ${s.error}` : `${s.monitor}`
 
+  const monitorError = Object.entries(error).find(([init, value]) => init!=='limit' && value!=='' )?.[1]
+
+    return (
+        <div className={s.counter}>
+            <div className={monitorStyle}>
+                { monitorError || counter}
+                <p></p>
             </div>
-            <div className={styles.buttonsContainer}>
-                <button></button>
-                <button></button>
+            <div className={s.buttonsContainer}>
+              <Button name={'add'} callback={increment} disabled={false}/>
+              <Button name={'reset'} callback={reset} disabled={false}/>
             </div>
 
         </div>
